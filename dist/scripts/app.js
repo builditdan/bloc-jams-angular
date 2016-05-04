43b4eb0c19380695750bc98939ef40e240500a35
builditdan <daniels111@gmail.com>
2016-05-04 08:30:12 -0400

    Add controllers for the landing page and collection

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -12,6 +12,7 @@
       $stateProvider
       .state('landing', {
         url: '/',
+        controller: 'LandingCtrl as landing',
         templateUrl: '/templates/landing.html'
 
       })
@@ -23,6 +24,7 @@
   
       .state('collection', {
         url: '/collection',
+        controller: 'CollectionCtrl as collection',
         templateUrl: '/templates/collection.html'
       });
  
