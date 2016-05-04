2b31ca011fe445ce6f781fbe2caf36f5710dbb33
builditdan <daniels111@gmail.com>
2016-05-04 12:48:47 -0400

    Add a Fixtures service for returning album information

---
+++ app/scripts/services/Fixtures.js
@@ -0,0 +1,46 @@
+(function() {
+  
+  function Fixtures() {
+    var Fixtures = {};
+    
+    var albumPicasso = {
+         title: 'Dan\'s Mix Tape',
+         artist: 'Various',
+         label: 'Cubism',
+         year: '1881',
+         albumArtUrl: 'assets/images/album_covers/01.png',
+         songs: [
+             { title: 'Blue', duration: '336', audioUrl: '/assets/music/blue2' } ,
+             { title: 'Green', duration: '320', audioUrl: '/assets/music/green2' } ,
+             { title: 'Red', duration: '175', audioUrl: '/assets/music/red2' },
+             { title: 'Pink', duration: '306', audioUrl: '/assets/music/pink1' }, 
+             { title: 'Magenta', duration: '343', audioUrl: '/assets/music/magenta1' }
+         ]
+     };
+
+     var albumMarconi = {
+         title: 'The Telephone',
+         artist: 'Guglielmo Marconi',
+         label: 'EM',
+         year: '1909',
+         albumArtUrl: 'assets/images/album_covers/20.png',
+         songs: [
+             { title: 'Hello, Operator?', duration: '1:01' },
+             { title: 'Ring, ring, ring', duration: '5:01' },
+             { title: 'Fits in your pocket', duration: '3:21' },
+             { title: 'Can you hear me now?', duration: '3:14' },
+             { title: 'Wrong phone number', duration: '2:15' }
+         ]
+     };
+    
+     Fixtures.getAlbum = function() {
+       return albumPicasso;
+     };
+    
+     return Fixtures;
+  }
+  
+  angular
+    .module('blocJams')
+    .factory('Fixtures', Fixtures);
+})
